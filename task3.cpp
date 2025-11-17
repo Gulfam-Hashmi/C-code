a78d7752eaacc6928beabadf6ebb6f10a0d8dadf
Gulfam-Hashmi <gulfamsattar000@gmail.com>
2025-11-17 09:13:21 -0800

    Add files via upload

--- task3.cpp
+++ task3.cpp
@@ -1,33 +1,211 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<array>
+
 using namespace std;
+class Course
+{
+    private:
+        string code;
+        string title;
+        float creditHours;
+    public:
+        string getCode()  //get and set for code
+        {
+            return code;
+        }
+        
+        void setCode(string _code)
+        {
+            code = _code;
+        }
+        
+        string getTitle()   //get and set for Title
+        {
+            return title;
+        }
+        
+        void setTitle(string _title)
+        {
+            title = _title;
+        }
+        
+        float getCreditHours()    //get and set for credithours
+        {
+            return creditHours;
+        }
+        
+        void setCreditHours(float _crHrs)
+        {
+            creditHours = _crHrs;
+        }
+        
+};
+
+class Student
+{
+	private:
+	string rollno;
+	string name;
+	float credithours;
+	Course data[3];
+	
+	public:
+	string getrollNo() //get and set for rollno
+	{
+		return rollno;
+	}
+	void setrollNo(string _rollno)
+	{
+		rollno = _rollno;
+	}
+	string getName() //get and set for name
+	{
+		return name;
+	}
+	void setName(string _name)
+	{
+		name = _name;
+	}
+	float getcreditHours() //get and set for credit hours
+	{
+		return credithours;
+	}
+	void setcreditHours(float _CH)
+	{
+		credithours = _CH;
+	}
+	//getter and setter for data arrray
+	Course getData(int i)
+	{
+		return data[i];
+	}
+	void setData(Course _data, int i)
+	{
+		data[i] = _data;
+	}
+};
+
+
+
+int maint1()
+{
+    array<Course,4>pf;
+	for(int i=0;i<4;i++)
+	{
+		string coursecode;
+		string title;
+		int credithours=0;
+		cout<<"Enter the code of Courseno "<<i+1<<": ";
+		cin>>coursecode;
+		cout<<"Enter the Title of Courseno "<<i+1<<": ";
+		cin>>title;
+		cout<<"Enter the CREDIT HOURS of Courseno "<<i+1<<": ";
+		cin>>credithours;
+		
+		pf[i].setCode(coursecode);
+		pf[i].setTitle(title);
+		pf[i].setCreditHours(credithours);
+		
+	}
+
+    for(int i=0;i<4;i++)
+	{
+	cout<<"                For Course"<<i+1<<endl;	
+    cout << "Details of PF are" << endl;
+    cout << "Code   : " << pf[i].getCode() << endl;
+    cout << "Title  : " << pf[i].getTitle() << endl;
+    cout << "Credits: " << pf[i].getCreditHours() << endl;
+	}
+    
+	return 0;
+}
+
+int main2()
+{
+	Course *pf = new Course[4];
+	for(int i=0;i<4;i++)
+	{
+		string coursecode;
+		string title;
+		int credithours=0;
+		cout<<"Enter the code of Courseno "<<i+1<<": ";
+		cin>>coursecode;
+		cout<<"Enter the Title of Courseno "<<i+1<<": ";
+		cin>>title;
+		cout<<"Enter the CREDIT HOURS of Courseno "<<i+1<<": ";
+		cin>>credithours;
+		
+		pf[i].setCode(coursecode);
+		pf[i].setTitle(title);
+		pf[i].setCreditHours(credithours);
+		
+	}
+
+    for(int i=0;i<4;i++)
+	{
+	cout<<"                For Course"<<i+1<<endl;	
+    cout << "Details of PF are" << endl;
+    cout << "Code   : " << pf[i].getCode() << endl;
+    cout << "Title  : " << pf[i].getTitle() << endl;
+    cout << "Credits: " << pf[i].getCreditHours() << endl;
+	}
+	return 0;
+} 
 
 int main()
 {
-	int max1,max2;
-	int six1 = 0,six2 = 0;
-	int sum1 = 0;
-	int sum2 = 0;
-	int i =1;
-	while(i <= 5)  //Round 1: Player 1 rolled 4, Player 2 rolled 6 
+	Student me;
+	string name,rollno;
+	float c_hours;  //hours will be the total in the semester
+	
+	cout<<"Enter the name of the student: ";               //input the data
+	getline(cin,name);
+	cout<<"Enter the roll no of the student: ";
+	getline(cin,rollno);
+	cout<<"Enter the credit hours of semester: ";
+	cin>>c_hours;
+	
+	me.setName(name);
+	me.setrollNo(rollno);
+	me.setcreditHours(c_hours);
+	
+	
+	
+	cout<<"Now you need to add the data for the course student is taking"<<endl;
+	for(int i=0;i<3;i++)
 	{
-		int num1,num2;
+		Course info;
+		string coursecode;
+		string title;
+		int credithours=0;
+		cout<<"Enter the code of Courseno "<<i+1<<": ";
+		cin>>coursecode;
+		cout<<"Enter the Title of Courseno "<<i+1<<": ";
+		cin>>title;
+		cout<<"Enter the TotalCREDIT HOURS of Courseno "<<i+1<<": ";
+		cin>>credithours;
 		
-		cout<<"Round "<<i<<": Player 1 rolled ";
-		cin >>  num1;
-		if(num1 > max1) max1 = num1;
-		if(num1 ==6) six1++; 
-		sum1 += num1;
+		info.setCode(coursecode);
+		info.setTitle(title);
+		info.setCreditHours(credithours);
 		
-		cout<<", Player 2 rolled ";
-		cin >>num2;
-		if(num2 > max2) max2 = num2;
-		if(num2 ==6) six2++; 
-		sum2 += num2;
-		cout<<endl; 				//Player 2 Total: 20, Sixes: 2, Max Roll: 6 
-		i++;
+		me.setData(info,i);   
 	}
 	
-	cout<< "Player total: " <<sum1<<", Sixes: "<<six1<<", Max Roll: "<<max1<<endl;
-	cout<< "Player total: " <<sum2<<", Sixes: "<<six1<<", Max Roll: "<<max2<<endl;
+	//ALL OUTPUT PORTION
+	cout<<"Name: "<<me.getName()<<endl;
+	cout<<"RollNo: "<<me.getrollNo()<<endl;
+	cout<<"Credit Hourse: "<<me.getcreditHours()<<endl;
+	 for(int i=0;i<3;i++)
+	{
+		Course info;
+		info = me.getData(i);
+	cout<<"                For Course"<<i+1<<endl;	
+    cout << "Details of Courses are" << endl;
+    cout << "Code   : " << info.getCode()<<endl;
+    cout << "Title  : " << info.getTitle() << endl;
+    cout << "Credits: " << info.getCreditHours() << endl;
+	}
 	return 0;
-}
+}	
