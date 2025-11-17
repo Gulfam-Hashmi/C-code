50b3d240de59ab292dfe1d2b52ac13996220324e
Gulfam-Hashmi <gulfamsattar000@gmail.com>
2025-11-17 09:13:16 -0800

    Add files via upload

--- task1.cpp
+++ task1.cpp
@@ -1,29 +1,77 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include<iostream>
+#include<string>
+#include<array>
 
 using namespace std;
 
-void drawW(int size) {
-    if (size < 3) size = 3;
-    if (size > 10) size = 10;
-	int collength = 4 * size - 3;
-    for (int row = 0; row < size; ++row) {
-        for (int col = 0; col < collength; ++col) {
-            if (col == row || col == 2 * size - 2 - row || col == 2 * size - 2 + row || col == collength -1 - row) {
-                cout << "*";
-            } else {
-                cout << " ";
-            }
+class Course
+{
+    private:
+        string code;
+        string title;
+        float creditHours;
+    public:
+        string getCode()
+        {
+            return code;
         }
-        cout << endl;
-    }
-}
+        
+        void setCode(string _code)
+        {
+            code = _code;
+        }
+        
+        string getTitle()
+        {
+            return title;
+        }
+        
+        void setTitle(string _title)
+        {
+            title = _title;
+        }
+        
+        float getCreditHours()
+        {
+            return creditHours;
+        }
+        
+        void setCreditHours(float _crHrs)
+        {
+            creditHours = _crHrs;
+        }
+        
+};
 
-int main() {
-    int size;
-    cout << "Enter size (3-10): ";
-    cin >> size;
-    drawW(size);
-    return 0;
+int main()
+{
+    array<Course,4>pf;
+	for(int i=0;i<4;i++)
+	{
+		string coursecode;
+		string title;
+		int credithours=0;
+		cout<<"Enter the code of Courseno "<<i+1<<": ";
+		cin>>coursecode;
+		cout<<"Enter the Title of Courseno "<<i+1<<": ";
+		cin>>title;
+		cout<<"Enter the CREDIT HOURS of Courseno "<<i+1<<": ";
+		cin>>credithours;
+		
+		pf[i].setCode(coursecode);
+		pf[i].setTitle(title);
+		pf[i].setCreditHours(credithours);
+		
+	}
+
+    for(int i=0;i<4;i++)
+	{
+	cout<<"                For Course"<<i+1<<endl;	
+    cout << "Details of PF are" << endl;
+    cout << "Code   : " << pf[i].getCode() << endl;
+    cout << "Title  : " << pf[i].getTitle() << endl;
+    cout << "Credits: " << pf[i].getCreditHours() << endl;
+	}
+    
+	return 0;
 }
