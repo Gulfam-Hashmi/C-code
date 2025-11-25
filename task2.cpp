04106fa20f89aa62ac1795f190ac9bc97a163aaf
Gulfam-Hashmi <gulfamsattar000@gmail.com>
2025-11-25 21:00:29 +0500

    Add files via upload

--- task2.cpp
+++ task2.cpp
@@ -1,113 +1,71 @@
-#include<iostream>
-#include<string>
-#include<array>
-
+#include <iostream>
 using namespace std;
-
-class Course
-{
-    private:
-        string code;
-        string title;
-        float creditHours;
-    public:
-        string getCode()
-        {
-            return code;
-        }
-        
-        void setCode(string _code)
-        {
-            code = _code;
-        }
-        
-        string getTitle()
-        {
-            return title;
-        }
-        
-        void setTitle(string _title)
-        {
-            title = _title;
-        }
-        
-        float getCreditHours()
-        {
-            return creditHours;
-        }
-        
-        void setCreditHours(float _crHrs)
-        {
-            creditHours = _crHrs;
-        }
-        
-};
-
-int maint1()
-{
-    array<Course,4>pf;
-	for(int i=0;i<4;i++)
-	{
-		string coursecode;
-		string title;
-		int credithours=0;
-		cout<<"Enter the code of Courseno "<<i+1<<": ";
-		cin>>coursecode;
-		cout<<"Enter the Title of Courseno "<<i+1<<": ";
-		cin>>title;
-		cout<<"Enter the CREDIT HOURS of Courseno "<<i+1<<": ";
-		cin>>credithours;
-		
-		pf[i].setCode(coursecode);
-		pf[i].setTitle(title);
-		pf[i].setCreditHours(credithours);
-		
-	}
-
-    for(int i=0;i<4;i++)
-	{
-	cout<<"                For Course"<<i+1<<endl;	
-    cout << "Details of PF are" << endl;
-    cout << "Code   : " << pf[i].getCode() << endl;
-    cout << "Title  : " << pf[i].getTitle() << endl;
-    cout << "Credits: " << pf[i].getCreditHours() << endl;
-	}
-	return 0;
-}
-
+	
 int main()
 {
-	Course *pf = new Course[4];
-	for(int i=0;i<4;i++)
+	int score = 0;
+	cout << "Lets start the game"<< endl;
+	for(int i = 0 ; i < 5 ; i++)
 	{
-		string coursecode;
-		string title;
-		int credithours=0;
-		cout<<"Enter the code of Courseno "<<i+1<<": ";
-		cin>>coursecode;
-		cout<<"Enter the Title of Courseno "<<i+1<<": ";
-		cin>>title;
-		cout<<"Enter the CREDIT HOURS of Courseno "<<i+1<<": ";
-		cin>>credithours;
+		int x;
+		int N1 = rand() %10;
+		int N2 = rand() %10;
+		int s = N1 + N2;
+		int m = N1 - N2;
+		int mul = N1 * N2;
+		cout << "N1: " <<N1<< "\t N2: "<< N2<<endl;
+		cout << "Sum: ";
+		cin >> x;
+		if(x == s)
+		{
+			score++;
+			cout<<endl;}
+		else
+		{ 
+			cout<<"try again"<<endl;
+			cin>>x;
+			if(x == s)
+			{
+				score++;
+				cout<<endl;
+			}
+		}
 		
-		pf[i].setCode(coursecode);
-		pf[i].setTitle(title);
-		pf[i].setCreditHours(credithours);
+		cout << "Difference: ";
+		cin >> x;
+		if(x == m)
+		{
+			score++;
+			cout<<endl;
+		}
+		else
+		{ 
+			cout<<"try again"<<endl;
+			cin>>x;
+			if(x == m)
+			{
+				score++;
+				cout<<endl;
+			}
+		}
 		
+		cout << "Product: ";
+		cin >> x;
+		if(x == mul)
+		{ score++; cout<<endl;}
+		else
+		{ 
+			cout<<"try again"<<endl;
+			cin>>x;
+			if(x == mul)
+			{
+				score++;
+				cout<<endl;
+			}
+		}
 	}
-
-    for(int i=0;i<4;i++)
-	{
-	cout<<"                For Course"<<i+1<<endl;	
-    cout << "Details of PF are" << endl;
-    cout << "Code   : " << pf[i].getCode() << endl;
-    cout << "Title  : " << pf[i].getTitle() << endl;
-    cout << "Credits: " << pf[i].getCreditHours() << endl;
-	}
-    
-	return 0;
 	
-	
+	cout<<"Total score: "<< score;
+		
 	return 0;
 }
-	
